9a808b2c8f93888a10567f1c50fe8bfabd50c2de
Vinali Samarasinghe <it21321368@my.sliit.lk>
2025-10-22 13:38:39 +0530

    Assign roles

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<IdentityUser,IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
