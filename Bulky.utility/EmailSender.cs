32bbd3bf65d3baadd13c0e357b9463910aeb5585
Vinali Samarasinghe <vinalisamarasinghe@gmail.com>
2025-11-10 19:20:15 +0530

    display user list

--- Bulky.utility/EmailSender.cs
+++ Bulky.utility/EmailSender.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,20 @@ namespace Bulky.utility
 {
     public class EmailSender: IEmailSender
     {
+        //public string SendGridSecret { get; set; }
+
+        //public EmailSender(IConfiguration _config)
+        //{
+        //    SendGridSecret =_config.GetValue<string>("SendGrid:SecretKey");
+        //}
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            // Here you would implement your email sending logic.
-            // For demonstration purposes, we'll just simulate an email being sent.
-            Console.WriteLine($"Sending Email to {email}");
-            Console.WriteLine($"Subject: {subject}");
-            Console.WriteLine($"Message: {htmlMessage}");
+            //var client = new SendGridClient(SendGridSecret);
+            //var from = new EmailAddress("hello@bulky.com", "bulky book");
+            //var to = new EmailAddress(email);
+            //var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+            //return client.SendEmailAsync(message);
+
             return Task.CompletedTask;
         }
     }
---
+++ BulkyWeb/wwwroot/js/user.js
@@ -0,0 +1,32 @@
+﻿var DataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+
+    DataTable = $('#tblData').DataTable({
+        "ajax": {
+            url: '/admin/user/getall'
+        },
+        "columns": [
+            { data: 'name', "width": "15%" },
+            { data: 'email', "width": "15%" },
+            { data: 'phoneNumber', "width": "15%" },
+            { data: 'company.name', "width": "15%" },
+            { data: 'role', "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return ` <div class="w-75 btn-group" role="group">
+                    <a href="/admin/company/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Update</a>
+                    
+                    </div>`
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}
+
+
