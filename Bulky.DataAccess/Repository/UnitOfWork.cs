74c0a077c5f96d93f344f527baa49223850f2143
Vinali Samarasinghe <vinalisamarasinghe@gmail.com>
2025-11-12 11:15:24 +0530

    Add Product Image Repository

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -19,6 +19,7 @@ namespace Bulky.DataAccess.Repository
         public IApplicationUserRepository ApplicationUser { get; private set; }
         public IOrderHeaderRepository OrderHeader { get; private set; }
         public IOrderDetailRepository OrderDetail { get; private set; }
+        public IProductImageRepository ProductImage { get; private set; }
         public UnitOfWork(ApplicationDbContext db) 
         {
             _db = db;
@@ -29,8 +30,9 @@ namespace Bulky.DataAccess.Repository
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
-            
-            
+            ProductImage = new ProductImageRepository(_db);
+
+
         }
         
 
